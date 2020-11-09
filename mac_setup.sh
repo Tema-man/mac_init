4273b8fd0bae425d1c35595a8fe2afbae906f5c5
Artemii Vishnevskii <artemii.vishnevskii@revolut.com>
2020-11-09 09:54:11 +0100

    added vimrc

--- mac_setup.sh
+++ mac_setup.sh
@@ -124,6 +124,10 @@ then
 fi
 cp .zshrc ~/.zshrc
 
+# -----------------------------------------------------------------------------
+# vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
 
 # -----------------------------------------------------------------------------
 # VSCode
